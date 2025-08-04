e7aaa9e64daa1b2b5ec8674acd1fb585c5a463f3
Dewmi2004 <imashadewmi557@gmail.com>
2025-08-04 13:35:49 +0530

    Library Apply Finish

--- src/config/FactoryConfiguration.java
+++ src/config/FactoryConfiguration.java
@@ -1,5 +1,26 @@
 package config;
 
-public class FactoryConfiguration {
+import entity.Customer;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
 
+
+public class FactoryConfiguration {
+    private static FactoryConfiguration factoryConfiguration;
+    private SessionFactory sessionFactory;
+
+private FactoryConfiguration() throws  HibernateException {
+    Configuration configuration = new Configuration().configure();
+    configuration.addAnnotatedClass(Customer.class);
+    sessionFactory = configuration.buildSessionFactory();
+}
+public static FactoryConfiguration getInstance() throws HibernateException {
+    return  factoryConfiguration == null ? factoryConfiguration =new FactoryConfiguration():factoryConfiguration;
+
+}
+public Session getSession() throws HibernateException {
+    return sessionFactory.openSession();
+}
 }
